4bd8fa4103dea28ce3363c568e9f9b12ca1be8f0
Victoria Warren <93948407+vmw19@users.noreply.github.com>
2022-04-26 22:48:56 -0400

    update class 4
    update class 4

--- 108 Backend/server.py
+++ 108 Backend/server.py
@@ -37,8 +37,66 @@ def get_cheapest():
 
     return json.dumps(solution)
 
-    #/api/catalog/total
-    # return the sume of all product's price
+#/api/catalog/total
+# return the sume of all product's price
+@app.route("/api/catalog/total")
+def get_total():
+    total = 0
+    for prod in mock_catalog:
+        total += prod["price"]
+
+        return json.dumps(total)
+
+#find a product based on the unique id
+@app.route("/api/product/<id>")
+def find_product(id):
+    for prod in mock_catalog:
+        if id == prod["_id"]:
+            return json.dumps(prod)
+
+#get the list of categories from the catalog
+#/api/products/categories
+#expectes: a list of strings containing the unique prods categories
+@app.route("/api/product/categories")
+def get_categories():
+    categories = []
+
+    #push all the categories into the list
+    for prod in mock_catalog:
+        cat = prod["category"]
+        if not cat in categories:
+            categories.append(cat)
+
+    return json.dumps(categories)
+
+#get all the products that belong to a specified category
+#/api/products/categories/heels
+@app.route("/api/product/category/<cat_name>")
+def get_by_category(cat_name):
+    results = []
+
+#add products whose category is equal to name
+for prod in mock_catalog:
+    if prod["category"].lower() == cat_name.lower():
+        results.append(prod)
+
+return json.dumps(results)
+
+#search by text inside the title
+#receive a text
+#return all product whose title contains the text
+@app.route("/api/products/search/<text>")
+def search_by_text(text):
+     results = []
+
+#search and add
+    for prod in mock_catalog:
+        title = prod["title"].lower()
+        if text.lower() in title:
+        results.append(prod)
+
+    return json.dumps(results)
+
 
 #start the server
 app.run(debug=True)
